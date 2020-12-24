7d2eaf64d12fd1b58f2ebf361f794b0230e75ccd
Krasimir Kanev <knev@mail.bg>
2020-12-24 21:05:59 +0200

    Learn

---
+++ ThinkingJava/src/Test/Operators.java
@@ -0,0 +1,8 @@
+package Test;
+
+public class Operators {
+    public static void main(String[] args) {
+        int n = 1;
+        System.out.printf("%d %d %d %d %d %d", n++, n, ++n, n--, n, --n);
+    }
+}
