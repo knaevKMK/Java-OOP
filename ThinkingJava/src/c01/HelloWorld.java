65c26c67829d9f45f8f32a5012d4a40df5d4065c
Krasimir Kanev <knev@mail.bg>
2021-01-09 14:44:13 +0200

    Practice

--- ThinkingJava/src/c01/HelloWorld.java
+++ ThinkingJava/src/c01/HelloWorld.java
@@ -3,13 +3,6 @@ package c01;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        /*
-        * The first Thinking in Java example program.
-         * Lists system information on current machine.
-         * @author Bruce Eckel
-         * @author http://www.BruceEckel.com
-         * @version 1.0
-         */
 
         System.out.println("Hello World");
     }
