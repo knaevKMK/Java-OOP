fc912acb76efa9753f1b458289f4da00d4152a73
Krasimir Kanev <knev@mail.bg>
2021-01-09 16:03:53 +0200

    Practice

--- ThinkingJava/src/c06/Chess/Main.java
+++ ThinkingJava/src/c06/Chess/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Chess x = new Chess();
         // Result print all main Constructors via super(). 3 extends class => 3 constructors
+        //main class need argument and Chess class gives it by default 11
+        // Chess still does not accept arguments in main (psvm)
     }
 }
